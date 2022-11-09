1650d6b36dfca03385933b84eb913ead1d6a4144
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-09 13:50:54 -0500

    nested resources, generated reviews controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "restaurants#index"
 
-  # Defines the root path route ("/")
-  root "articles#index"
-  get "/restaurants", to: 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
